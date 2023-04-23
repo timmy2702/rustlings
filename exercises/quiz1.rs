0769d6c09de675fb58992e1b89e4843ee7249ae1
Tim Nguyen <timmy2702@users.noreply.github.com>
2023-04-23 22:07:35 +0100

    Finished upto structs

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,10 +10,12 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+const APPLE_COST: u8 = 2;
+const APPLE_DISCOUNT_COST: u8 = 1;
+fn calculate_price_of_apples(amount: u8) -> u8 {
+    return if amount <= 40 { amount * APPLE_COST } else { amount * APPLE_DISCOUNT_COST };
+}
 
 // Don't modify this function!
 #[test]
